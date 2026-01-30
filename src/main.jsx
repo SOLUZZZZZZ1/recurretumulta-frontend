99021a12aef9b1d3256813d8c18c183e55025048
mediazion <soluzziona@gmail.com>
2026-01-30 20:26:18 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <App />
+      </Router>
+    </HelmetProvider>
   </React.StrictMode>
 );
