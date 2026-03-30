df2906607c59d8b74737079c2ab838c60f8f5091
mediazion <soluzziona@gmail.com>
2026-03-30 12:50:55 +0200

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import logo from "/rtm-navbar.png";
+import logo from "/rtm LOGO.png";
 
 export default function Navbar() {
   const { pathname } = useLocation();
